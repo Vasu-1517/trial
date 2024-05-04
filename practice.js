a65bf679fccfea7e6af9c811bf416e161acd0309
Vasundhara P <payasamvasundhara@gmail.com>
2024-05-04 23:05:31 +0530

    commiting the code0

---
+++ practice.js
@@ -0,0 +1,26 @@
+//console.log("Hii guru")
+// function display(){
+//     var a=9;
+//     console.log(a);
+//     {
+//         let b=12;
+//         console.log(b);
+//         console.log(a);
+        
+//     }
+//     //console.log(b);
+// }
+// display();
+
+
+function  display(){
+    const a=5
+    {
+        const b=6
+
+    }
+    //console.log(b)
+}
+display()
+var user = prompt("Please enter the data")
+console.log(user)
